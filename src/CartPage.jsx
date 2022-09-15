99deee8738f30fce2e6ddd16e79546cfc45caeb3
SimranVerma23 <simranverma13022002@gmail.com>
2022-09-15 23:12:44 +0530

    Cart-Page

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import CartList from "./CartList";
-import CartRow from "./CartRow";
 
 
 function CartPage() {
   return (
     <div className="flex flex-col">
       < CartList />
-      < CartRow />
+    
     </div>
   );
 
